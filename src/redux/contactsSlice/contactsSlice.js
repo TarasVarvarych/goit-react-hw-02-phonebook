f28b93f24244a80593f8b1fb96351edb5f3c03f3
TarasVarvarych <varvarychtaras@gmail.com>
2023-05-14 10:44:45 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice/contactsSlice.js
+++ src/redux/contactsSlice/contactsSlice.js
@@ -15,7 +15,7 @@ const contactsSlice = createSlice({
       state.push(action.payload);
     },
     deleteContact(state, action) {
-      state.filter(contact => contact.id !== action.payload);
+      return state.filter(contact => contact.id !== action.payload);
     },
   },
 });
